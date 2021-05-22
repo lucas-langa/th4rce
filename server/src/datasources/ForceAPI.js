992f261b7264f187c277b4d2aa53f5556a0de2b0
Lucas <llanga@student.wethinkcode.co.za>
2021-05-22 17:56:54 +0200

    pagination complete

--- server/src/datasources/ForceAPI.js
+++ server/src/datasources/ForceAPI.js
@@ -6,26 +6,19 @@ class ForceAPI extends RESTDataSource {
         this.baseURL = 'https://swapi.dev/api/';
     }
 
-    async getAllPeople() {
-        const response = await this.get('people/');
+    async getAllPeople(page) {
+        const response = await this.get('people/', { page:page });
         return Array.isArray(response.results)
             ? response.results.map(person => this.personReducer(person))
             : [];
     }
 
     async getPersonByName(name) {
-        // https://swapi.dev/api/people/?search=r2
         try {
             const response = await this.get(`people`, { search: name });
             let {results} = response;
             results = results[0];
             if (!response || response.results.length == 0) throw new Error("no data");
-            // let planetID = results['homeworld']; 
-            // let planetID = this.resolvePlanetId(results['homeworld']);
-            // planetID.substring(29, planetID.length - 1);
-            // const planet = await this.getPlanet(planetID);
-            // results['homeworld'] = planet['name'];
-            // console.log(results)
             return this.personReducer(results);
         } catch (error) {
             return []
@@ -42,15 +35,16 @@ class ForceAPI extends RESTDataSource {
 
     async personReducer(person) {
         try {
-            const {name} =await this.getPlanet(this.resolvePlanetId(person.homeworld));
+            const {name} = await this.getPlanet(this.resolvePlanetId(person.homeworld));
             return {
                 name: person.name,
                 height: person.height,
                 gender: person.gender,
+                mass: person.mass,
                 homeworld: name               
             };
         } catch (error) {
-            
+            return []
         }
     }
 }
--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,17 +1,15 @@
 async function person(_, { name }, { dataSources }) {
-    // console.log(name)
     try {
         const data = dataSources.ForceAPI.getPersonByName(name);
         if (!data) return []
         return data
     } catch (error) {
-        console.log(error+"error at person")
         if (error.extensions.response.status === 404) return null;
     }
 }
 
-async function allPeople(_, __, { dataSources }) {
-    const people = await dataSources.ForceAPI.getAllPeople();
+async function allPeople(_, { page=1 }, { dataSources }) {
+    const people = await dataSources.ForceAPI.getAllPeople( page );
     return people;
 };
 

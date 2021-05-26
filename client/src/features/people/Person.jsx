0a75ae623647e73fca0e4e65ce541b3df606628b
Lucas <llanga@student.wethinkcode.co.za>
2021-05-27 00:00:13 +0200

    oh no

--- client/src/features/people/Person.jsx
+++ client/src/features/people/Person.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { getThemById } from "./personSlice";
 import { Link } from 'react-router-dom';
+import starwarsImg from '../../images/starwars.png'
 
 function Person(props) {
     const { name } = props.match.params;
@@ -12,14 +13,14 @@ function Person(props) {
         return (<p>Loading....</p>)
     return (
         <div className="card" style={{width:400}}>
-            <img className="card-img-top" alt="would be star wars character if i were good at redux" src="./starwars.png"/>
+            <img className="card-img-top" src={starwarsImg} alt="would be star wars character if i were good at redux"/>
             <div className="card-body">
-                <h4 className="card-title">{yours.name}</h4>
-                <p className="card-text">{yours.height}</p>
-                <p className="card-text">{yours.weight}</p>
-                <p className="card-text">{yours.homeworld}</p>
-                <p className="card-text">{yours.gender}</p>
-                <p className="card-text">{yours.mass}</p>
+                <h4 className="card-title">Name: {yours.name}</h4>
+                <p className="card-text">Height: {yours.height}</p>
+                <p className="card-text">Weight: {yours.weight}</p>
+                <p className="card-text">Homeworld: {yours.homeworld}</p>
+                <p className="card-text">Gender: {yours.gender}</p>
+                <p className="card-text">Mass: {yours.mass}</p>
                 <Link to={"/"}>back to your characters</Link>
             </div>
         </div>

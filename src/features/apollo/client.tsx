d480ecebe0c514657a3f50a7cbad8bef51799d8d
Lucas <llanga@student.wethinkcode.co.za>
2021-05-27 03:25:37 +0200

    hope

--- src/features/apollo/client.tsx
+++ src/features/apollo/client.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: process.env.APOLLO_SERVER || 'http://localhost:4000/graphql',
+    uri: process.env.APOLLO_SERVER,
     cache: new InMemoryCache()
 });
 

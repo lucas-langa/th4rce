37f3ba106137767d8adc9892135c60f215c5c7dc
Lucas <llanga@student.wethinkcode.co.za>
2021-05-27 03:43:30 +0200

    ?

--- src/features/apollo/client.tsx
+++ src/features/apollo/client.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: process.env.APOLLO_SERVER,
+    uri: "https://young-wave-08967.herokuapp.com/graphql",
     cache: new InMemoryCache()
 });
 

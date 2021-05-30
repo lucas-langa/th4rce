35189fba0ff589aff94b7b23d4888116b9f1dd9a
Lucas <llanga@student.wethinkcode.co.za>
2021-05-30 20:23:44 +0200

    hmmm

--- src/features/apollo/client.tsx
+++ src/features/apollo/client.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: (!process.env.NODE_ENV || process.env.NODE_ENV === "development") ? "http://localhost:4000/graphql" : process.env.REACT_APP_GRAPHQL_SERVER_URL,
+    uri: (!process.env.NODE_ENV || process.env.NODE_ENV === "development") ? "http://localhost:4000/graphql" : "https://young-wave-08967.herokuapp.com/graphql",
     cache: new InMemoryCache()
 });
 
